5b036ece37f16b78676a7c073c512ce391b8dc65
Imam-007 <imimam8409@gmail.com>
2024-11-08 16:02:43 +0530

    making api call

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+  const URL = "https://api.spoonacular.com/recipes/complexSearch";
+  const API_KEY = "414157de5cc740c68fbc849a4733e686";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchFood();
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
