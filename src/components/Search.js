c41e4c869839feeec4d7352e414ce98680d589db
Imam-007 <imimam8409@gmail.com>
2024-11-10 10:18:40 +0530

    added styling on search button and input feild

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
+import styles from "./search.module.css";
 
 export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
   const URL = "https://api.spoonacular.com/recipes/complexSearch";
-  const API_KEY = "414157de5cc740c68fbc849a4733e686";
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
   useEffect(() => {
     async function fetchFood() {
-      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
-      const data = await res.json();
-      console.log(data.results);
-      setFoodData(data.results);
+      try {
+        const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        console.log(data.results);
+        setFoodData(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }
     fetchFood();
   }, [query]);
 
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+        className={styles.input}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
